878c32caa4b797272c19b9a91aea8939403aea2b
しみさん <74240481+shimisan-polyglot@users.noreply.github.com>
2020-12-15 20:33:37 +0900

    Add files via upload

---
+++ java/org/ranc/memberlistapp/MemberlistAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ranc.memberlistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberlistAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
